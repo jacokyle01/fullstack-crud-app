876e236ed934c9d7b59ce088a4e3ae8b1decf81c
Kyle Jacobson <jacokyle01@gmail.com>
2024-02-18 18:13:51 -0600

    setup backend with maven

---
+++ Backend/match10s/src/main/java/com/tennis/match10s/Match10sApplication.java
@@ -0,0 +1,13 @@
+package com.tennis.match10s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Match10sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Match10sApplication.class, args);
+	}
+
+}
---
+++ Backend/match10s/src/test/java/com/tennis/match10s/Match10sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennis.match10s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Match10sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
